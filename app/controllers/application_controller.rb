91a6accc224fcf5844b6472654007bdc4037b799
alexawesomecode <alexawesomecode@gmail.com>
2020-06-25 21:44:18 +0000

    add routing exception and test for invalid records

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+
+def route_not_found
+	render json: {"msg": "not found"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :bodyparts, only: [:index] do
+  resources :bodyparts, only: [:index]  do
 	  resources :measures, only: [ :index, :create, :show]
   end
 
   get '/bodyparts/:name/measures'  => 'measures#index'
   get '/bodyparts/:bodypart_id/progress' => 'measures#progress'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  match '*unmatched', to: 'application#route_not_found', via: :all
+
 end
